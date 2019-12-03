bcefb62be715156228b7b04f7e26f34c97e7f35f
Laky Stevanovic <laky_vr@hotmail.com>
2019-12-03 13:24:19 +0100

    Dodate 3 rute preko compojure: 1.osnovna -> localhost:8005 2.zbogom -> localhost:8005/zbogom 3.greska -> bilo koja sto nije ove 2, primer: localhost:8005/nestotest

--- src/clojure_app/core.clj
+++ src/clojure_app/core.clj
@@ -1,37 +1,45 @@
 (ns clojure-app.core
   (:require [ring.adapter.jetty :as webserver]
-            [ring.middleware.reload :refer [wrap-reload]])
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]
+
+            )
   )
+(defn goodbye
+  "Goodbye"
+  [request]
+  {:status 200
+   :headers {}
+   :body "<h1>Zbogom</h1>"})
 
 (defn welcome
   "A ring handler to process all requests for the web server.  If a request is for something other than then an error message is returned"
   [request]
-  (if (= "/" (:uri request))
-    {:status 200
-     :body "<h1>Hello, Clojure World</h1>
-            <p>Test.</p>"
-     :headers {}}
-    {:status 404
-     :body "<h1>Nedostupna ruta</h1>
-            <p>></p>"
-     :headers {}}))
+  {:status 200
+     :body "<h1>Zdravoo, Clojure World</h1>
+            <p>Test.2</p>"
+     :headers {}})
+
+(defroutes app
+           (GET "/" [] welcome)
+           (GET "/zbogom" [] goodbye)
+           (not-found " <h1>greska</h1>")
+           )
 
 
 (defn -dev-main
   "Jednostavna web aplikacija koristi Ring i Jetty, dodat middleware wrap--reload za automatsko prepoznavanje promena u kodu"
   [port-number]
-  (webserver/run-jetty
-    (wrap-reload #'welcome)
-    {:port  (Integer. port-number)
-     :join? false}))
+  (webserver/run-jetty (wrap-reload #'app)
+    {:port  (Integer. port-number)}))
 
 (defn main
   "Ovde nema automatskog detektovanja promene koda"
   [port-number]
   (webserver/run-jetty
-    welcome
+    app
     {:port  (Integer. port-number)}))
 
 
-
-(-dev-main 8000)
+(-dev-main 8005)
